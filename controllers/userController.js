f6d21ecf5891a9934f3982f3cbd8531e1c314324
Kyle <kyleyee522@gmail.com>
2024-04-15 21:08:50 -0700

    created API to get a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,4 +11,15 @@ module.exports = {
 			res.status(500).json(error);
 		}
 	},
+	async getOneUser(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params.userId });
+			if (!user) {
+				return res.status(404).json({ message: 'No user with that ID' });
+			}
+			res.json(user);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	},
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getUsers } = require('../../controllers/userController');
+const { getUsers, getOneUser } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers);
+
+router.route('/:userId').get(getOneUser);
 
 module.exports = router;
